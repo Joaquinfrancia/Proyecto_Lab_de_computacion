3f608f9acb7f1258c15041b35d2c8dd9b01e2b18
Facundov1 <82005265+Facundov1@users.noreply.github.com>
2021-07-21 15:08:49 -0300

    Add files via upload
    estructura base

---
+++ index.js
@@ -0,0 +1,5 @@
+// window.onload=() =>{
+//     fetch("https://v6.exchangerate-api.com/v6/36661222e39d847792391c2d/latest/USD")
+//     .then(response => response.json())
+//     .then(data=>console.log(data))
+// }
